578c342a47251c6773037175b3304da90c7f62d6
IgorFerreira <igorferreirabr@gmail.com>
2019-08-04 10:30:26 -0300

    removido lombok de log para permitir recompile automatico

--- src/main/java/techlab/GreetingResource.java
+++ src/main/java/techlab/GreetingResource.java
@@ -5,17 +5,19 @@ import javax.ws.rs.Path;
 import javax.ws.rs.Produces;
 import javax.ws.rs.core.MediaType;
 
-import lombok.extern.slf4j.Slf4j;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
-@Slf4j
 @Path("/hello")
 public class GreetingResource {
+
+    protected final Logger log = LoggerFactory.getLogger(this.getClass());
 
     @GET
     @Produces(MediaType.TEXT_PLAIN)
     public String hello() {
 
-        log.info("\n\n logging... Hello World ! \n\n");
+        log.info("\n\n logging... Hello World ! alterado 3\n\n");
 
         return "hello";
     }
