d688e8c628128aabe368f8be69ca5c04c3ab33b3
IgorFerreira <igorferreirabr@gmail.com>
2019-08-04 10:18:10 -0300

    adicionado lombok, e log, porem o re-compile para de funcionar

--- src/main/java/techlab/GreetingResource.java
+++ src/main/java/techlab/GreetingResource.java
@@ -5,12 +5,18 @@ import javax.ws.rs.Path;
 import javax.ws.rs.Produces;
 import javax.ws.rs.core.MediaType;
 
+import lombok.extern.slf4j.Slf4j;
+
+@Slf4j
 @Path("/hello")
 public class GreetingResource {
 
     @GET
     @Produces(MediaType.TEXT_PLAIN)
     public String hello() {
+
+        log.info("\n\n logging... Hello World ! \n\n");
+
         return "hello";
     }
 }
